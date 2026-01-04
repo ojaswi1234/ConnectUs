e1e44e4e7a49bad4870fb20412e85e0248ee2792
ojaswideep2020 <ojaswideep2020@gmail.com>
2026-01-04 23:21:11 +0000

    modified grapghql

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
-import { createServer } from "http"
-import { createSchema, createYoga } from "graphql-yoga"
+import { createServer } from "http";
+import { createSchema, createYoga, createPubSub } from "graphql-yoga"; // Added createPubSub
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT || 4000;
+
+const PORT = process.env.PORT || 3000;
 const messages = [];
+const pubSub = createPubSub(); // Initialize PubSub instance
+
 createServer(
   createYoga({
     schema: createSchema({
       typeDefs: /* GraphQL */ `
-        type Message{
+        type Message {
           id: ID!
           user: String!
           content: String!
@@ -17,27 +20,37 @@ createServer(
           messages: [Message!]
         }
         type Mutation {
-          postMessage(user: String!, content: String!): ID!}
+          postMessage(user: String!, content: String!): ID!
+        }
+        type Subscription {
+          messageAdded: Message! # New Subscription type
+        }
       `,
       resolvers: {
         Query: {
           messages: () => messages,
         },
         Mutation: {
-          postMessage: (parent, {user, content}) => {
-            const id = messages.length;
-            messages.push({
-              id,
-              user,
-              content,
-            });
+          postMessage: (parent, { user, content }) => {
+            const id = String(messages.length);
+            const newMessage = { id, user, content };
+            messages.push(newMessage);
+            
+            // Publish the message to the 'MESSAGE_ADDED' channel
+            pubSub.publish("MESSAGE_ADDED", { messageAdded: newMessage });
+            
             return id;
-
-          }
-        }
+          },
+        },
+        Subscription: {
+          messageAdded: {
+            // Subscribe to the channel
+            subscribe: () => pubSub.subscribe("MESSAGE_ADDED"),
+          },
+        },
       },
     }),
-  }),
+  })
 ).listen(PORT, () => {
-  console.info(`GraphQL Yoga is listening on http://localhost:${PORT}/graphql`)
-})
+  console.info(`GraphQL Yoga is listening on http://localhost:${PORT}/graphql`);
+});
